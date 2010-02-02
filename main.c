0607811c26d643558bf8cf8dfebf51a1e60e5d7e
Greg Leclercq <ggl@0x80.net>
2010-02-02 16:42:42 +0100

    Update example in main.c .
    Signed-off-by: Greg Leclercq <ggl@0x80.net>

--- main.c
+++ main.c
@@ -14,9 +14,26 @@
 
 #include <stdlib.h>
 #include <errno.h>
+#include <syslog.h>
 
 #include "network_server.h"
 
+/** Simple echo callback.
+ *
+ */
+void
+client_callback_do_request(struct ev_loop *loop, ev_io *w,
+		struct buffer *bufwrite,
+		struct buffer *bufread,
+		int *done)
+{
+	struct peer_client *client = (struct peer_client *) w;
+	buffer_append(bufwrite,
+			buffer_get_data(bufread), buffer_get_size(bufread));
+	buffer_clear(bufread);
+	*done = 1;
+	ev_io_start(loop, &client->watcher_write);
+}
 
 int
 main(int argc, char **argv)
@@ -25,11 +42,17 @@ main(int argc, char **argv)
 	int port = 12345;
 	int backlog = 256;
 
-	struct server *server = server_new(256);
+	struct server *server = server_new(2);
 	if (server == NULL)
 		exit(errno);
 
-	int err = server_init(server, SERVER_NONBLOCKING);
+	struct server_callbacks callbacks = {
+		.log = syslog,
+		.accept = NULL,
+		.do_request = client_callback_do_request
+	};
+
+	int err = server_init(server, &callbacks, SERVER_NONBLOCKING);
 	if (err) goto fail_server_init;
 	server_listen(server, host, port, backlog);
 
